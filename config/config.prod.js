9998cdeab0f6a7132acf9d3ea2335d0a07b087ee
Hari Narasimhan <harin76@gmail.com>
2015-10-23 00:45:20 +0530

    changes for heroku deployment

--- config/config.prod.js
+++ config/config.prod.js
@@ -7,7 +7,7 @@ config.env = "prod";
 
 //config.db.host = "your.production.url";
 //config.db.dbName = "insightProd";
-//config.db.url = "your.production.url"
+config.db.url = process.env.db || 'localhost';
 
 // Most default values are for the dev env, so we need not do anything here
 module.exports = config;
