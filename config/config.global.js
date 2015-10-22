95aec5f46d04e30b0d2937cbb08312b5401bdde8
Hari Narasimhan <harin76@gmail.com>
2015-10-23 01:22:41 +0530

    port changes

--- config/config.global.js
+++ config/config.global.js
@@ -5,7 +5,7 @@ var config = {app:{}, db:{}};
 // set the default encvironment
 config.env = "dev";
 
-config.app.port = process.env.port || 3030;
+config.app.port = process.env.PORT || 3030;
 
 config.db.host = "localhost";
 config.db.dbName = "insightDev";
