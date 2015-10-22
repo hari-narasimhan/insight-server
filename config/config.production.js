9cdd6d1510963d481180e53be2dfc699bf478c64
Hari Narasimhan <harin76@gmail.com>
2015-10-23 01:15:43 +0530

    changed db config

--- config/config.production.js
+++ config/config.production.js
@@ -7,7 +7,7 @@ config.env = "production";
 
 //config.db.host = "your.production.url";
 //config.db.dbName = "insightProd";
-config.db.url = process.env.db || 'localhost';
+config.db.url = "mongodb://insight:admin1234@ds061721.mongolab.com:61721/insight";
 
 // Most default values are for the dev env, so we need not do anything here
 module.exports = config;
